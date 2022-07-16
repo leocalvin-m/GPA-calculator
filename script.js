6b55a184e678d535c43df64876a02b2e843fb567
Num-21 <leocalvin.m@gmail.com>
2022-07-16 14:31:27 +0530

    first commit

--- script.js
+++ script.js
@@ -4,10 +4,12 @@ const tabContainer = document.querySelector(".operations-tab");
 const tabStyle = document.querySelectorAll(".tab-style");
 const content = document.querySelectorAll(".operations-content");
 const addsub = document.querySelector(".btn-sub");
-console.log(addsub);
+const input = document.querySelector(".inputs_container");
+console.log(input);
 class App {
     constructor() {
-        tabContainer.addEventListener('click', this._tabSwitch.bind(this));
+        tabContainer.addEventListener('click', this._tabSwitch);
+        addsub.addEventListener('click', this._renderInputFields);
     }
     _tabSwitch(e) {
         tabStyle.forEach(function (ele) {
@@ -20,6 +22,25 @@ class App {
         document.querySelector(`.tab-${e.target.dataset.tab}`).classList.add("content-active");
 
 
+
+    }
+    _renderInputFields() {
+        const html = `
+                                <div class="sub-container">
+                            <input placeholder="subject" type="text" class="sub inp">
+                            <select name="grade" id="grade" class="grade">
+                                <option value="">grade</option>
+                                <option>O</option>
+                                <option>S</option>
+                                <option>A</option>
+                                <option>B</option>
+                                <option>C</option>
+                                <option>D</option>
+                            </select>
+                            <input type="text" placeholder="credits" class="grade inp">
+                        </div>
+        `;
+        input.insertAdjacentHTML('afterend', html);
 
     }
 }
